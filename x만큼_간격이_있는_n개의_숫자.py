f005145ab05b183635cda3d75fec0f8056c36969
sjj995 <54494622+sjj995@users.noreply.github.com>
2021-09-16 20:34:48 +0900

    09.16

---
+++ x만큼_간격이_있는_n개의_숫자.py
@@ -0,0 +1,12 @@
+def solution(x, n):
+    answer = []
+    if (x>0):
+        for i in range(x,x*n+1,x):
+            answer.append(i)
+    elif (x<0):
+        for i in range(x,x*n-1,x):
+            answer.append(i)
+    else:
+        for i in range(n):
+            answer.append(0)
+    return answer
---
+++ 숫자_문자열과_영단어.py
@@ -0,0 +1,7 @@
+def solution(s):
+    dict = {0:'zero',1:'one',2:'two',3:'three',4:'four',5:'five',6:'six',
+           7:'seven',8:'eight',9:'nine'}
+    for key,val in dict.items():
+        s = s.replace(val,str(key))
+
+    return int(s)
